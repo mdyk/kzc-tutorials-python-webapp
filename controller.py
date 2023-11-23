c7c073d4d9b9973f8e73bf7ace11135ec6ac456d
Michal Dyk <michal.dyk@flyingatom.eu>
2023-11-23 20:10:53 +0100

    - kontrakt dla tworzenia nowych postów

--- controller.py
+++ controller.py
@@ -1,9 +1,25 @@
 from fastapi import FastAPI
-
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
+class Post(BaseModel):
+    title: str
+    text: str
+    author: str
+
+
+# """
+# {
+#     "title" : "",
+#     "text": "",
+#     "author": ""
+# }
+
+# """
+
+
 @app.post("/blog/post")
-def create_new_post(title: str, post_text: str):
+def create_new_post(new_post: Post):
     pass
